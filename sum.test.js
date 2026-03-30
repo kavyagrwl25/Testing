e51f0ca23a18a0c74d560901a43ecfd4e6f876c3
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2026-03-31 03:29:55 +0530

    feat: organize sum tests and add toEqual tests

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,22 @@
 import sum from "./sum.js"
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1,2)).toBe(3);
+describe('sum', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1,2)).toBe(3);
+    });
+    test('adds -5 + -5 to equal -10', () => {
+        expect(sum(-5,-5)).toBe(-10);
+    });
+    test('adds 2.5 + 3.5 to equal 6', () => {
+        expect(sum(2.5,3.5)).toBe(6);
+    });
+});
+
+
+test('adding positive numbers is not zero', () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0);
+    }
+  }
 });
